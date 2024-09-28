cae04560a72b5dbb37727968dc030fa4995b20a0
Patrick_Hieu <hieunguyenthanh204@gmail.com>
2024-09-28 16:25:33 +0700

    Upload file  answer and rename exercise 6.1 and 6.2

--- Week_1/Lab01/Exercise61.java
+++ Week_1/Lab01/Exercise61.java
@@ -1,6 +1,6 @@
 package Lab01;
 import javax.swing.JOptionPane;
-public class ChoosingOption {
+public class Exercise61 {
     public static void main(String[] args) {
         int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
 
