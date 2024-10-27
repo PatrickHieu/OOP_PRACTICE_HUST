a51bbde1e4b06266ad9b78c9fc683bea7d4efd7e
Patrick_Hieu <hieunguyenthanh204@gmail.com>
2024-10-27 10:47:17 +0700

    Upload java code

--- Lab_02/AimsProject/src/Aims.java
+++ Lab_02/AimsProject/src/Aims.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Aims {
+    public static void main(String[] args) {
+        // Create Cart
+        Cart cart = new Cart();
+
+        //Create DVD
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 87, 18.99f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // Total Cost
+        System.out.println("Total Cost is: $" + cart.totalCost());
+
+        // Delete and show Total Cost
+        cart.removeDigitalVideoDisc(dvd2);
+        System.out.println("Final Total Cost is: $" + cart.totalCost());
+    }
 }
--- Lab_02/AimsProject/src/Cart.java
+++ Lab_02/AimsProject/src/Cart.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
-
 public class Cart {
+    private final DigitalVideoDisc[] items = new DigitalVideoDisc[20];
+    private int itemOrdered = 0;
+
+    // Create DVD
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (itemOrdered < 20) {
+            items[itemOrdered] = disc;
+            itemOrdered++;
+            System.out.println("Completed Added DVD");
+        } else {
+            System.out.println("The cart is almost full");
+        }
+    }
+
+    // Delete DVD out of Cart
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < itemOrdered; i++) {
+            if (items[i] == disc) {
+                for (int j = i; j < itemOrdered - 1; j++) {
+                    items[j] = items[j + 1];
+                }
+                items[itemOrdered - 1] = null;
+                itemOrdered--;
+                System.out.println("Complete Removed DVD");
+                return;
+            }
+        }
+        System.out.println("Could not find the DVD");
+    }
+
+    // Total Cost
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < itemOrdered; i++) {
+            total += items[i].getCost();
+        }
+        return total;
+    }
 }
